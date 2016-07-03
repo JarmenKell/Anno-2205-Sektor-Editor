7ddbc97d9d9c5d35ae67e27907fd162df6fd0c2f
JarmenKell <github@c6h12o6.eu>
2016-07-03 23:17:02 +0200

    möp

---
+++ Anno 2205 Sektor Editor/MainWindow.xaml.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.Drawing;
+using System.IO;
+
+namespace Anno_2205_Sektor_Editor
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        a6m gamedata;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            openfile_Click(null, null);
+        }
+
+        private void openfile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Sektor |*gamedata*.data";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                FileStream fs = new FileStream(openFileDialog.FileName, FileMode.Open);
+                byte[] data = new byte[(int)fs.Length];
+                fs.Read(data, 0, (int)fs.Length);
+                fs.Close();
+
+                string text = openFileDialog.FileName;
+                while (text.Length > 42) text = text.Substring(1);
+                filename.Text = text;
+
+                gamedata = new a6m(data);
+
+                bytes_list.Items.Clear();
+                gamedata.blocks.ForEach(block =>
+                {
+                    bytes_list.Items.Add((new TextBlock()).Text = block.Length + " Bytes");
+                });
+
+                map_list.Items.Clear();
+                gamedata.maps.ForEach(map =>
+                {
+                    map_list.Items.Add((new TextBlock()).Text = map.descriptor + " (" + map.length / 1024 + "kB)");
+                });
+            }
+        }
+        private void savefile_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Sektor |*gamedata*.data";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                FileStream fs = new FileStream(saveFileDialog.FileName, FileMode.Create);
+                byte[] data = gamedata.getBytes();
+                fs.Write(data, 0, data.Length);
+                fs.Close();
+                MessageBox.Show("gespeichert");
+            }
+        }
+
+        private void save_map_Click(object sender, RoutedEventArgs e)
+        {
+            if (gamedata == null || map_list.SelectedIndex == -1) return;
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Als png speichern|*.png";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                Window save = new Window();
+                save.Width = 218;
+                save.Height = 32;
+                save.Title = "Bitte warten";
+                save.Show();
+                gamedata.create_map(map_list.SelectedIndex).Save(saveFileDialog.FileName);
+                save.Close();
+            }
+        }
+
+        private void save_allmaps_Click(object sender, RoutedEventArgs e)
+        {
+            if(gamedata == null) return;
+            System.Windows.Forms.FolderBrowserDialog folderDialog = new System.Windows.Forms.FolderBrowserDialog();
+            if (folderDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                Window save = new Window();
+                save.Width = 218;
+                save.Height = 32;
+                save.Title = "Bitte warten";
+                save.Show();
+                for (int i = 0; i < gamedata.maps.Count; i++)
+                {
+                    gamedata.create_map(i).Save(folderDialog.SelectedPath + "\\" + i + "_" + gamedata.maps[i].descriptor + ".png");
+                }
+                save.Close();
+            }
+        }
+
+        private void open_map_Click(object sender, RoutedEventArgs e)
+        {
+            int index = map_list.SelectedIndex;
+            if (gamedata == null || index == -1) return;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Aus png einlesen|*.png";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Bitmap bitmap = new Bitmap(openFileDialog.FileName);
+                map map = gamedata.maps[index];
+                if (bitmap.Size.Width != map.size || bitmap.Size.Height != map.size)
+                {
+                    MessageBox.Show("Die " + map.descriptor + " muss eine größe von " + map.size + "x" + map.size + " pixel haben");
+                }
+                else
+                {
+                    Window save = new Window();
+                    save.Width = 218;
+                    save.Height = 32;
+                    save.Title = "Bitte warten";
+                    save.Show();
+                    gamedata.read_map(index, bitmap);
+                    save.Close();
+                    MessageBox.Show("Die " + map.descriptor + " wurde eingelesen");
+                }
+            }
+        }
+
+        private void export_Click(object sender, RoutedEventArgs e)
+        {
+            if (gamedata == null || bytes_list.SelectedIndex == -1) return;
+            SaveFileDialog saveFile = new SaveFileDialog();
+            if (saveFile.ShowDialog() == true)
+            {
+                FileStream file = File.Create(saveFile.FileName);
+                byte[] block = gamedata.blocks[bytes_list.SelectedIndex];
+                file.Write(block, 0, block.Length);
+                file.Close();
+            }
+        }
+
+        //private void button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    SaveFileDialog saveFile = new SaveFileDialog();
+        //    if (saveFile.ShowDialog() == true)
+        //    {
+        //        int length = 2048 * 4096 + 128;
+        //        byte[] dds = new byte[length];
+
+        //        dds[0] = 0x44;
+        //        dds[1] = 0x44;
+        //        dds[2] = 0x53;
+        //        dds[3] = 0x20;
+        //        dds[4] = 0x7C;
+        //        dds[8] = 0x07;
+        //        dds[9] = 0x30;
+        //        dds[10] = 0x08;
+        //        dds[13] = 0x10;
+        //        dds[17] = 0x10;
+        //        dds[21] = 0x20;
+        //        dds[28] = 0x01;
+        //        dds[76] = 0x20;
+        //        dds[80] = 0x04;
+        //        dds[84] = 0x44;
+        //        dds[85] = 0x58;
+        //        dds[86] = 0x54;
+        //        dds[87] = 0x31;
+        //        dds[109] = 0x10;
+        //        dds[111] = 0x04;
+
+        //        for (int i = 128; i < length;)
+        //        {
+        //            dds[i++] = 0xF0;
+        //            dds[i++] = 0x87;
+        //            dds[i++] = 0xEF;
+        //            dds[i++] = 0x7F;
+        //            dds[i++] = 0xFF;
+        //            dds[i++] = 0xFF;
+        //            dds[i++] = 0xFF;
+        //            dds[i++] = 0xFF;
+        //        }
+
+        //        FileStream file = File.Create(saveFile.FileName);
+        //        file.Write(dds, 0, length);
+        //        file.Close();
+        //    }
+        //}
+    }
+}
---
+++ Anno 2205 Sektor Editor/a6m.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+using Microsoft.Win32;
+
+namespace Anno_2205_Sektor_Editor
+{
+    struct map
+    {
+        public string descriptor;
+        public short size;
+        public int length;
+        public string type;
+        public object data;
+
+        public map(string descriptor, short size, int length, string type, object data)
+        {
+            this.descriptor = descriptor;
+            this.size = size;
+            this.length = length;
+            this.type = type;
+            this.data = data;
+        }
+    }
+
+    class a6m
+    {
+        public List<byte[]> blocks = new List<byte[]>();
+        public List<map> maps = new List<map>();
+
+        public a6m (byte[] data)
+        {
+            int pos = 0;
+            while (data[pos] != 130 || data[pos + 1] != 192 || data[pos + 2] != 128 || data[pos + 3] != 4) pos++;
+            pos += 4;
+            next_block(pos, 0, data);
+
+            byte[,,] heightmap = new byte[2048, 2048, 2];
+            for (int i = 2047; i >= 0; i--)
+            {
+                for (int j = 0; j < 2048; j++)
+                {
+                    heightmap[j, i, 0] = data[pos++];
+                    heightmap[j, i, 1] = data[pos++];
+                }
+                pos += 2;
+            }
+            maps.Add(new map("Heightmap", 2048, 2049 * 4096, "Grün 1 Byte, Blau 1 Byte", heightmap));
+
+            int block_beginn = pos;
+            while (data[pos] != 128 || data[pos + 1] != 128 || data[pos + 2] != 128 || data[pos + 3] != 64) pos++;
+            pos += 4;
+            next_block(pos, block_beginn, data);
+
+            byte[,] sectionmap = new byte[1024, 1024];
+            for (int i = 1023; i >= 0; i--)
+            {
+                for (int j = 0; j < 1024; j++)
+                {
+                    sectionmap[j, i] = data[pos++];
+                }
+            }
+            maps.Add(new map("Sectionmap", 1024, 1024 * 1024, "Graustufen 1 Byte", sectionmap));
+
+            block_beginn = pos;
+            while (data[pos] != 128 || data[pos + 1] != 128 || data[pos + 2] != 128 || data[pos + 3] != 64) pos++;
+            pos += 4;
+            next_block(pos, block_beginn, data);
+
+            sectionmap = new byte[1024, 1024];
+            for (int i = 1023; i >= 0; i--)
+            {
+                for (int j = 0; j < 1024; j++)
+                {
+                    sectionmap[j, i] = data[pos++];
+                }
+            }
+            maps.Add(new map("Sectionmap", 1024, 1024 * 1024, "Graustufen 1 Byte", sectionmap));
+
+            block_beginn = pos;
+            while (data[pos] != 128 || data[pos + 1] != 128 || data[pos + 2] != 128 || data[pos + 3] != 8) pos++;
+            pos += 4;
+            next_block(pos, block_beginn, data);
+
+            byte[] temp = new byte[128 * 1024];
+            Buffer.BlockCopy(data, pos, temp, 0, 128 * 1024);
+            BitArray bitmap = new BitArray(temp);
+            pos += 128 * 1024;
+            maps.Add(new map("Bitmap", 1024, 128 * 1024, "Schwarz Weiss", bitmap));
+
+            block_beginn = pos;
+            while (data[pos] != 128 || data[pos + 1] != 128 || data[pos + 2] != 128 || data[pos + 3] != 1) pos++;
+            pos += 4;
+            next_block(pos, block_beginn, data);
+
+            byte[,] minimap = new byte[128, 128];
+            for (int i = 127; i >= 0; i--)
+            {
+                for (int j = 0; j < 128; j++)
+                {
+                    minimap[j, i] = data[pos++];
+                }
+            }
+            maps.Add(new map("Minimap", 128, 128 * 128, "Graustufen 1 Byte", minimap));
+
+            block_beginn = pos;
+            while (data[pos] != 128 || data[pos + 1] != 128 || data[pos + 2] != 128 || data[pos + 3] != 8) pos++;
+            pos += 4;
+            next_block(pos, block_beginn, data);
+
+            temp = new byte[128 * 1024];
+            Buffer.BlockCopy(data, pos, temp, 0, 128 * 1024);
+            bitmap = new BitArray(temp);
+            pos += 128 * 1024;
+            maps.Add(new map("Bitmap", 1024, 128 * 1024, "Schwarz Weiss", bitmap));
+
+            next_block(data.Length, pos, data);
+        }
+
+        private void next_block(int pos, int block_beginn, byte[] data)
+        {
+            byte[] temp = new byte[pos - block_beginn];
+            Buffer.BlockCopy(data, block_beginn, temp, 0, pos - block_beginn);
+            blocks.Add(temp);
+        }
+
+        public Byte[] getBytes()
+        {
+            MemoryStream data = new MemoryStream(1024 * 1024 * 32);
+            int h;
+            for (h = 0; h < maps.Count; h++)
+            {
+                data.Write(blocks[h], 0, blocks[h].Length);
+                byte[] map = new byte[0];
+                int pos = 0;
+                switch (maps[h].descriptor)
+                {
+                    case "Heightmap":
+                        byte[,,] Heightdata = (byte[,,])maps[h].data;
+                        map = new byte[maps[h].length];
+                        for (int i = 2047; i >= 0; i--)
+                        {
+                            for (int j = 0; j < 2048; j++)
+                            {
+                                map[pos++] = Heightdata[j, i, 0];
+                                map[pos++] = Heightdata[j, i, 1];
+                            }
+                            map[pos++] = 0;
+                            map[pos++] = 0;
+                        }
+                        break;
+
+                    case "Minimap":
+                    case "Sectionmap":
+                        byte[,] Sectiondata = (byte[,])maps[h].data;
+                        map = new byte[maps[h].length];
+                        for (int i = maps[h].size - 1; i >= 0; i--)
+                        {
+                            for (int j = 0; j < maps[h].size; j++)
+                            {
+                                map[pos++] = Sectiondata[j, i];
+                            }
+                        }
+                        break;
+
+                    case "Bitmap":
+                        BitArray Bitdata = (BitArray)maps[h].data;
+                        map = new byte[maps[h].length];
+                        Bitdata.CopyTo(map, 0);
+                        break;
+                }
+                data.Write(map, 0, map.Length);
+            }
+            data.Write(blocks[h], 0, blocks[h].Length);
+            data.Capacity = (int)data.Length;
+            return data.GetBuffer();
+        }
+
+        public Bitmap create_map(int index)
+        {
+            map map = maps[index];
+            Bitmap bitmap = new Bitmap(map.size, map.size);
+
+            switch (map.descriptor)
+            {
+                case "Heightmap":
+                    byte[,,] Heightdata = (byte[,,])map.data;
+                    for (int i = 0; i < 2048; i++)
+                    {
+                        for (int j = 0; j < 2048; j++)
+                        {
+                            bitmap.SetPixel(i, j, Color.FromArgb(0, Heightdata[i, j, 0], Heightdata[i, j, 1]));
+                        }
+                    }
+                    break;
+
+                case "Minimap":
+                case "Sectionmap":
+                    byte[,] Sectiondata = (byte[,])map.data;
+                    for (int i = 0; i < map.size; i++)
+                    {
+                        for (int j = 0; j < map.size; j++)
+                        {
+                            bitmap.SetPixel(i, j, Color.FromArgb(Sectiondata[i, j], Sectiondata[i, j], Sectiondata[i, j]));
+                        }
+                    }
+                    break;
+
+                case "Bitmap":
+                    BitArray Bitdata = (BitArray)map.data;
+                    int k = 0;
+                    for (int i = 1023; i >= 0; i--)
+                    {
+                        for (int j = 0; j < 1024; j++)
+                        {
+                            bitmap.SetPixel(j, i, Bitdata.Get(k++) ? Color.White : Color.Black);
+                        }
+                    }
+                    break;
+            }
+            return bitmap;
+        }
+
+        public void read_map(int index, Bitmap bitmap)
+        {
+            map map = maps[index];
+
+            switch (map.descriptor)
+            {
+                case "Heightmap":
+                    byte[,,] Heightdata = (byte[,,])map.data;
+                    for (int i = 0; i < 2048; i++)
+                    {
+                        for (int j = 0; j < 2048; j++)
+                        {
+                            Heightdata[i, j, 0] = bitmap.GetPixel(i, j).G;
+                            Heightdata[i, j, 1] = bitmap.GetPixel(i, j).B;
+                        }
+                    }
+                    break;
+
+                case "Minimap":
+                case "Sectionmap":
+                    byte[,] Sectiondata = (byte[,])map.data;
+                    for (int i = 0; i < map.size; i++)
+                    {
+                        for (int j = 0; j < map.size; j++)
+                        {
+                            Sectiondata[i, j] = bitmap.GetPixel(i, j).R;
+                        }
+                    }
+                    break;
+
+                case "Bitmap":
+                    BitArray Bitdata = (BitArray)map.data;
+                    int k = 0;
+                    for (int i = 1023; i >= 0; i--)
+                    {
+                        for (int j = 0; j < 1024; j++)
+                        {
+                            Bitdata.Set(k, bitmap.GetPixel(j, i) == Color.White);
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
